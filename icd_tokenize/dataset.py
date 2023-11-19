1bc43a1366a8ef418440416476d82cdf2c979db4
Eddie Lin <eddielin0926@gmail.com>
2023-11-19 19:22:43 +0800

    feat: :sparkles: update dataset columns name

--- icd_tokenize/dataset.py
+++ icd_tokenize/dataset.py
@@ -6,9 +6,15 @@ import os
 import numpy as np
 import pandas as pd
 import pyarrow
-from datasets import Dataset, Features, Sequence, Value
+from datasets import ClassLabel, Dataset, Features, Sequence, Value
 
 pyarrow.PyExtensionType.set_auto_load(True)
+
+
+icd_df = pd.read_csv("icd_tokenize/data/original-icd.csv")
+icd_dict = dict(zip(icd_df["diagnosis"], icd_df["ICD1"]))
+icd_set = set(icd_dict.values())
+class_labels = ClassLabel(num_classes=len(icd_set), names=list(icd_set))
 
 
 def generate_data():
@@ -40,6 +46,8 @@ def generate_data():
 
                 input_data = list(filter(lambda x: x != "", input_data))
                 target_data = list(filter(lambda x: x != "", target_data))
+                icd_data = [icd_dict.get(icd, "R97") for icd in target_data]
+                class_data = [class_labels.str2int(icd) for icd in icd_data]
 
                 yield {
                     "year": year + 1911,
@@ -52,7 +60,9 @@ def generate_data():
                     "serial_no": row["流水號"],
                     "catalog": i,
                     "inputs": input_data,
-                    "labels": target_data,
+                    "results": target_data,
+                    "icds": icd_data,
+                    "encodes": class_data,
                 }
 
 
@@ -68,11 +78,11 @@ features = Features(
         "serial_no": Value("int32"),
         "catalog": Value("int32"),
         "inputs": Sequence(Value("string")),
-        "labels": Sequence(Value("string")),
+        "results": Sequence(Value("string")),
+        "icds": Sequence(Value("string")),
+        "encodes": Sequence(class_labels),
     }
 )
 
 dataset = Dataset.from_generator(generate_data, features=features)
-print(dataset[0])
-print(dataset.features)
 dataset.push_to_hub("eddielin0926/chinese-icd")
